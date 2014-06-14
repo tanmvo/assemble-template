4647f6eebd4f6308361d6d705c75c1991344200c
Tan Vo <tan.m.vo@gmail.com>
2014-06-14 14:01:00 +1000

    Optimised grunt tasks, removed browser-sync and utilise grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,110 +1,60 @@
+'use strict';
+
 module.exports = function(grunt) {
-	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// watch tasks
-		watch: {
-			hbs: { 
-				files: 'templates/**/*.hbs',
-				tasks: ['assemble:dev']
-			},
-			copy: {
-				files: './assets/img/*.{gif,jpg,png}',
-				tasks: ['copy:dev'],
-				options: {
-					event: ['added', 'changed']
-				}
-			}
-		},
-		// assemble config
 		assemble: {
-			// Add development option - produce .html files for testing 
-			dev: {
-				options: {
-					flatten:  true,
-					layout: 'templates/layouts/default.hbs',
-					partials: ['templates/partials/*.hbs'],
-					production: false
-				},
-				files: {
-					'wip/': ['templates/pages/*.hbs']
-				}
-			},
-			// Produce files for distribution 
-			prod: {
-				options: {
-					flatten:  true,
-					layout: 'templates/layouts/default.hbs',
-					partials: ['templates/partials/*.hbs'],
-					production: true
-				},
-				files: {
-					'dist/': ['templates/pages/*.hbs']
-				},
-			}
-		},
-		// Clean directories before assemble
-		clean: {
-			dev: ['wip/*'],
-			prod: ['dist/*']
-		},
-		// copy img/ folder into new directory
-		copy: {
-			dev: {
-				files: [
-					{
-						expand: true,
-						cwd: 'assets/',
-						src: 'img/**',
-						dest: 'wip/'
-					}
-				]
-			}
-		},
-		// Browser-sync to view files on web browser
-		browserSync: {	
-			bsFiles: {
-				src: [
-					'wip/*.html',
-					'img/**'
-				]
-			},
 			options: {
-				watchTask: true,
-				server: {
-					baseDir: './wip/',
-					index: false,
-					ghostMode: {
-						clicks: false,
-						scroll: true,
-						links: false,
-						forms: false
-					}
-				}
+		        flatten: true,
+		        prettify: {
+		          indent: 2,
+		          condense: true,
+		          newlines: true
+		        },
+		        assets: 'assets/',
+		        helpers: 'templates/helpers/*.js',
+		        partials: 'templates/includes/*.hbs',
+		        layoutdir: 'templates/layouts',
+		        layout: 'default.hbs',
+		    },
+	      	email001: {
+	      		files: {'dist/email-001': ['email-001/index.hbs']},
+	      		options: {
+	      			partials: 'email-001/product.hbs',
+	      			data: 'email-001/*.json'
+	      		}
+	      	},
+		},
+		clean: ['dist/**/*'],
+		connect: {
+			options: {
+				port: 8000,
+				livereload: 35729,
+				hostname: 'localhost',
+				open: {appName: 'Google Chrome'},
+				base: ['dist/']
+			},
+			livereload: {
+				
+			}
+		},
+		watch: {
+			options: {
+					livereload: true,
+				},
+			hbs: {
+				files: ['email-001/*.hbs', 'templates/**/*.hbs'],
+				tasks: ['assemble'],
+				
 			},
 		},
-		replace: {
-			prod: {
-				src: ['dist/*.html'],
-				overwrite: true,
-				replacements: [{
-					from: 'img/',
-					to: 'http://taosdirect.taoscreative.com.au/cimages/2214562f47d1ad0561eb8b221efcdde4/'
-				}]
-			}
-		}
 	});
-
-	// Load plugins
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('assemble');
-	grunt.loadNpmTasks('grunt-browser-sync');
-	grunt.loadNpmTasks('grunt-text-replace');
-	
-	// Default task
-	grunt.registerTask('default', ['clean:dev', 'assemble:dev', 'copy:dev', 'browserSync', 'watch']);
-	grunt.registerTask('dist', ['clean:prod', 'assemble:prod', 'replace:prod']);
-
+	// Load npm plugins to provide necessary tasks.
+  	grunt.loadNpmTasks('assemble');
+  	grunt.loadNpmTasks('grunt-contrib-clean');
+  	grunt.loadNpmTasks('grunt-contrib-connect');
+  	grunt.loadNpmTasks('grunt-contrib-watch');
+  	// Default task to be run.
+  	grunt.registerTask('default', ['clean', 'assemble']);
+  	grunt.registerTask('test', ['clean', 'assemble', 'connect', 'watch']);
 };
